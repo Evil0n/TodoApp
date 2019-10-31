66855ea1cee3b33d9442ba7e1b2510a8a70c18ea
Evil0n <48404800+Evil0n@users.noreply.github.com>
2019-10-31 15:21:28 +0300

    First Test Vue

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was create by {{ user.id }}</p>
+    <p>{{ getEventByID(2) }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["getEventByID"]),
+    ...mapState(["user", "categories"])
+    /*  mapState({
+                user: "user",
+                categories: "categories"
+              })   я не понял как он это вот так сократил */
+  }
+};
+</script>
+
+<style scoped></style>
